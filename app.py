1aac342e9c3258d98f3b90705d109e932f3118c1
oziy01 <oziyan02@gmail.com>
2026-05-08 05:32:39 -0700

    Update app.py

--- app.py
+++ app.py
@@ -51,4 +51,5 @@ def delete(task_id):
     return redirect(url_for("index"))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
